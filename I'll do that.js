b5b344916e624bcb90dc5acd6dd5d3abaa32a0fd
ggsacoi <118943639+ggsacoi@users.noreply.github.com>
2024-12-27 15:51:43 +0100

    sac

--- I'll do that.js
+++ I'll do that.js
@@ -3,12 +3,15 @@ addEventListener('DOMContentLoaded', () => {
     const buttonright = document.getElementById("B");
     const slider = document.querySelector(".photos");
     const img = document.querySelector('.ones');
-    const scrollAmount = img.width;
-    let currentTranslateX = 0;
+    let scrollAmount = img.offsetWidth;
+    if(img.style.width === '40vw' || img.style.width === '100vw'){
+        scrollAmount = img.offsetWidth;
+    };
     
     buttonright.addEventListener("click", ()=> {
         if (slider) { 
             slider.scrollLeft += scrollAmount;
+
         }
     });
 
